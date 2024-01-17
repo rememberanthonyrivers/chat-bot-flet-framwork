9f90075e42429c04b35afe6edb2b885f0224c5ce
tony <hireriversanthony@gmail.com>
2024-01-17 07:23:40 -0500

    first commit

---
+++ chat_app.py
@@ -0,0 +1,17 @@
+import flet as ft
+
+def main(page: ft.Page):
+    chat = ft.Column() #container or box for text field 
+    new_message = ft.TextField() # area where msgs will be displayed 
+
+    def send_click(e):
+        chat.controls.append(ft.Text(new_message.value))
+        new_message.value = ""
+        page.update()
+
+    page.add(
+        chat,
+        ft.Row(controls=[new_message, ft.ElevatedButton("Send", on_click=send_click)]),
+    )
+
+ft.app(target=main) 
